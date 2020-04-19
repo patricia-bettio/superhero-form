f8ea4cf68cde98a0c6b6070a1e71d17702bf0be3
Patricia Bettio <pfbettio@gmail.com>
2020-04-19 16:56:32 +0200

    basic post

--- superhero_form.js
+++ superhero_form.js
@@ -1,4 +1,6 @@
-//import {endpoint, apiKey} from "./modules/settings";
+import {endpoint, apiKey} from "./modules/settings";
+
+
 
 const form = document.querySelector("form");
 //make some things part of the global scope so that I can access them from the console, when dealing with parcel: ***to debug in the console
@@ -39,14 +41,53 @@ form.setAttribute("novalidate", true);
 form.addEventListener("submit", (e) => {
     //console.log("submited")
     e.preventDefault();
-    console.log(form.checkValidity());
-    if (form.checkValidity()) {
-        //send to api
-       console.log("okay")
+    //set the form to true, is valid
+    let validForm = true;
+    //console.log(form.checkValidity());
+    //first clear all invalid fields
+    const formElements = document.querySelectorAll("input");
+    formElements.forEach((oneElement) => {
+        oneElement.classList.remove("invalid");
+        
+    });  
+
+    //2.at least one checkbox
+    //transform it into an array
+    const checkBoxes = [ ... form.querySelectorAll(`[name=powers]`)];
+    //".checked" to find out wich were selected. default
+    const checkedBox = checkBoxes.filter((e) => e.checked);
+    //console.log(checkBoxes)
+    //console.log(checkedBox)
+
+    //4. select the alert for the checkboxes
+    const powersFail = form.querySelector("fieldset p");
+    //3. specify critereas
+    if (checkedBox.length===0){
+    //if the lenght is 0 the form is not valid
+    validForm = false;
+    //show error message
+    powersFail.classList.remove("hidden");
     } else {
-        const formElements = document.querySelectorAll("input");
+    //keep it hidden
+    powersFail.classList.add("hidden");
+    }
+    //1.all input validate
+    if (form.checkValidity() && validForm) {
+        //send to api
+    postSuperHero({
+        //creating an object to pass along with that function all the data,
+        real_name: form.elements.real_name.value,
+        alias: form.elements.alias.value,
+        dob: form.elements.dob.value, //fix
+        location: form.elements.location.value,
+        enemies: form.elements.enemies.value,
+        //fav_color: form.elements.fav_color.value,
+        powers: [] //fix
+    });
+    } else {
+        //if there is an error, we go through them
         formElements.forEach((oneElement) => {
-            oneElement.classList.remove("invalid");
+           
             if(!oneElement.checkValidity()){
                 oneElement.classList.add("invalid");
             }
@@ -54,3 +95,18 @@ form.addEventListener("submit", (e) => {
 
         }
     });
+
+function postSuperHero(payload){
+        const postData = JSON.stringify(payload);
+        fetch(endpoint, {
+        method: "post",
+        headers: {
+        "Content-Type": "application/json; charset=utf-8",
+        "x-apikey": apiKey,
+        "cache-control": "no-cache",
+  },
+  body: postData,
+})
+  .then(res => res.json())
+  .then(data => console.log(data));
+};
